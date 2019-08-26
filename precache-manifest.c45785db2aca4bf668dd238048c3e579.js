caac3b275596d2be69049642e47227868e8240b1
Robson <robson-durval@hotmail.com>
2019-08-26 17:06:59 -0300

    Updates

---
+++ precache-manifest.c45785db2aca4bf668dd238048c3e579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6551a5ac5cbd57e63db2d8aacefc7a",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aac90b58352f4a4d5625",
+    "url": "/react-monsters-rolodex/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react-monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "aac90b58352f4a4d5625",
+    "url": "/react-monsters-rolodex/static/js/main.793da2f2.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
